2c864971228598698784b150a3a50adc0bd984fb
sakir <sakirovvladislav832@gmail.com>
2025-10-20 15:39:45 +0700

    Добавьте файлы проекта.

---
+++ DB/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingProgram.DB
+{
+    public class MyDbContext : DbContext
+    {
+        private protected string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DatingDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlServer(connectionString));
+        }
+
+        
+    }
+}
